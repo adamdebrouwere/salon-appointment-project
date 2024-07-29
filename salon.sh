264245ec66a8e6850bd3842d2fff76e66e939dad
Adam D <adamdebrouwere@hotmail.com>
2024-07-29 19:06:56 +0000

    Save progress: 1.1

--- salon.sh
+++ salon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
-echo -e "\n~~~ Salon Appointments Assistant ~~~\n"
-echo -e "\nWhich of our lovely services would you like?\n"
+echo -e "\n~~~~~ MY SALON ~~~~~"
+  echo -e "\nWelcome to My Salon, how can I help you?\n"
 
 #service selector
 SELECT_SERVICE() {
@@ -10,84 +10,82 @@ SELECT_SERVICE() {
   then
     echo -e "\n$1"
   fi
+  
 
-  SERVICES_DISPLAY;
-  APPOINTMENT_CREATOR;
-  EXIT;
-}
 
-SERVICES_DISPLAY() {
   ALL_SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
   #display numbered list of services
-  echo "$ALL_SERVICES" | while read SERVICE_ID _ NAME _
+  echo "$ALL_SERVICES" | while read -r SERVICE_ID _ NAME
   do
     echo -e "$SERVICE_ID) $NAME"
   done
+
+  APPOINTMENT_CREATOR
+  EXIT
 }
 
 APPOINTMENT_CREATOR() {
   
   read SERVICE_ID_SELECTED
+  IS_ON_SERVICES_MENU=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
 
-  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ || -z $IS_ON_SERVICES_MENU ]]
   then
-    SELECT_SERVICE "That's not a service we offer. Please try again.\n"
-  else
-    IS_ON_SERVICES_MENU=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
-        
-    if [[ -z $IS_ON_SERVICES_MENU ]]
-    then
-      SELECT_SERVICE "That's not a service we offer. Please try again.\n"
-    else
-      echo -e "\nWhat is your phone number?"
-      read CUSTOMER_PHONE
+    SELECT_SERVICE "I could not find that service. What would you like today?"
+    return
+  fi 
+  
+  
+  echo -e "\nWhat's your phone number?"
+  read CUSTOMER_PHONE
 
-      if [[ ! $CUSTOMER_PHONE =~ ^[0-9-]+$ ]]
-      then
-        SELECT_SERVICE "Please enter an appropriate phone number."
-      else
-      
-        IS_CUSTOMER=$($PSQL "SELECT * FROM customers WHERE phone='$CUSTOMER_PHONE'")
-        
-        if [[ -z $IS_CUSTOMER ]]
-        then
-          echo -e "\nWhat is your name?"
-          read CUSTOMER_NAME
-        
-          if [[ -z $CUSTOMER_NAME ]]
-          then
-            SELECT_SERVICE "Please enter something as you name."
-          else 
-            INSERT_NEW_CLIENT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
-          fi
-        fi
-
-        GET_SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED") 
-        SERVICE_NAME_FORMATTED=$(echo $GET_SERVICE_NAME | sed -E 's/^ *| *$//g')
-        GET_CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
-        CUSTOMER_NAME_FORMATTED=$(echo $GET_CUSTOMER_NAME | sed -E 's/^ *| *$//g')
-
-        echo -e "\nWhat time would you like to come in for your $SERVICE_NAME_FORMATTED, $CUSTOMER_NAME_FORMATTED?"
-        read SERVICE_TIME
-
-        if [[ ! $SERVICE_TIME =~ ^[0-9:]+( )?(am|pm|AM|PM)?$ ]]
-        then
-          SELECT_SERVICE "Please input an appropriate time."
-        else
-          GET_CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
-          
-          INSERT_APPOINTMENT_TIME=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($GET_CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-
-          echo -e "\nI have put you down for a $SERVICE_NAME_FORMATTED at $SERVICE_TIME, $CUSTOMER_NAME_FORMATTED."
-        
-        fi
-      fi
-    fi
+  if [[ ! $CUSTOMER_PHONE =~ ^[0-9-]+$ ]]
+  then
+    echo "Please enter an appropriate phone number."
+    read CUSTOMER_PHONE
   fi
+    
+  IS_CUSTOMER=$($PSQL "SELECT * FROM customers WHERE phone='$CUSTOMER_PHONE'")
+      
+  if [[ -z $IS_CUSTOMER ]]
+  then
+    echo -e "\nI don't have a record for that phone number, what's your name?"
+    read CUSTOMER_NAME
+  
+    #while [[ -z $CUSTOMER_NAME ]]
+    #do
+      #echo "Please enter something as you name."
+      #read CUSTOMER_NAME
+    #done
+
+    INSERT_INTO_DB=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+  fi
+
+  GET_SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED") 
+  SERVICE_NAME_FORMATTED=$(echo $GET_SERVICE_NAME | sed -E 's/^ *| *$//g')
+  GET_CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  CUSTOMER_NAME_FORMATTED=$(echo $GET_CUSTOMER_NAME | sed -E 's/^ *| *$//g')
+
+  echo -e "\nWhat time would you like your $SERVICE_NAME_FORMATTED, $CUSTOMER_NAME_FORMATTED?"
+  read SERVICE_TIME
+
+  #while [[ ! $SERVICE_TIME =~ ^[0-9:]+( )?(am|pm|AM|PM)?$ ]]
+  #do
+    #echo "Please input an appropriate time."
+    #read SERVICE_TIME
+  #done
+
+  
+  GET_CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  
+  INSERT_INTO_APPOINTMENTS=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($GET_CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+  echo -e "\nI have put you down for a $SERVICE_NAME_FORMATTED at $SERVICE_TIME, $CUSTOMER_NAME_FORMATTED."
+  EXIT 
 }
 
 EXIT() {
-  echo -e "\nThank you for stopping by! Please come back soon!"
+  #echo -e "\nThank you for stopping by! Please come back soon!"
   exit
 }
 
